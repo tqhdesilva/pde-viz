88717d0603e9a23cc9298ff04531f0e8e0b80f69
tqhdesilva <tdesilvaqihao@gmail.com>
2020-05-13 08:24:03 -0700

    changed wave equation to use a stiff solver

--- wave_equation.jl
+++ wave_equation.jl
@@ -1,6 +1,7 @@
 module WaveEquation
 using DifferentialEquations
 using DiffEqOperators
+using LinearAlgebra
 
 Base.@kwdef struct WaveEquation1D
     icₓ::Function
@@ -21,32 +22,44 @@ function solve(d::WaveEquation1D)
     Nₜ = length(t)
     u₀ = d.ic.(x)
     du₀ = d.icₓ.(x)
-    A = zeros(Nₓ, Nₓ)
-    A[1, :] = [[-2, 1]; [0 for i = 1:Nₓ - 2]]
-    A[end, :] = [[0 for i = 1:Nₓ - 2]; [1, -2]]
+    U₀ = [u₀; du₀]
+    A₁₁ = zeros(Nₓ, Nₓ)
+    A₁₂ = Matrix{Float64}(I, Nₓ, Nₓ)
+    A₂₂ = zeros(Nₓ, Nₓ)
+
+    A₂₁ = zeros(Nₓ, Nₓ)
+    A₂₁[1, :] = [[-2, 1]; [0 for i = 1:Nₓ - 2]]
+    A₂₁[end, :] = [[0 for i = 1:Nₓ - 2]; [1, -2]]
     for i = 2:Nₓ - 1
-        A[i, :] = [
+        A₂₁[i, :] = [
             [0 for j = 1:i - 2];
             [1, -2, 1];
             [0 for j = 1:Nₓ - i - 1]
         ]
     end
-    A = A .* (1 / d.Δx^2) .* d.c²
+    A₂₁ = A₂₁ .* (1 / d.Δx^2) .* d.c²
 
-    function waveequation!(ddu, du, u, p, t)
-        ddu[:] = A * u + [
+    A = [
+        [A₁₁ A₁₂];
+        [A₂₁ A₂₂]
+    ]
+
+    function waveequation!(du, u, p, t)
+        du[:] = A * u + [
             [d.leftBoundary];
             [0 for i = 1:Nₓ - 2];
-            [d.rightBoundary]
+            [d.rightBoundary];
+            [0 for i = 1:Nₓ]
         ]
     end
 
-    prob = SecondOrderODEProblem{true}(waveequation!, du₀, u₀, (0.0, d.T))
-    sol = DifferentialEquations.solve(prob)
+    prob = ODEProblem(waveequation!, U₀, (0.0, d.T))
+    sol = DifferentialEquations.solve(prob, KenCarp4())
     solarray = zeros(Nₓ, Nₜ + 1)
     solarray[:, 1] = u₀
     for (i, tᵢ) in enumerate(t)
-        uᵢ = sol(tᵢ).x[2]
+        Uᵢ = sol(tᵢ)
+        uᵢ = Uᵢ[1:Integer(length(Uᵢ) / 2)]
         solarray[:, i + 1] = uᵢ
     end
     return solarray
